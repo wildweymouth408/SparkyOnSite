1ba18aa4c6c92a31cdafa15ce26c7ceff725455d
wildweymouth408 <wildweymouth408@gmail.com>
2026-02-21 20:43:45 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: `
           (function() {
